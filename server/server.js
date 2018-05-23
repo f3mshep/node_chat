4c084f738aae8dac8e33a34500f49d838216a8e0
f3mshep <superbiscuit@gmail.com>
2018-05-23 13:30:41 -0700

    messing with web sockets

--- server/server.js
+++ server/server.js
@@ -15,6 +15,25 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
   console.log('New user connected')
+
+  socket.emit('newEmail', {
+    from: "mike@example.com",
+    text: "Whaddup G"
+  });
+
+  socket.on('createMessage', (message)=>{
+    console.log('New Message: ', message)
+  });
+
+  socket.emit('newMessage', {
+    from: "me",
+    text: "wheeee"
+  })
+
+  socket.on('createEmail', (newEmail)=>{
+    console.log('CreateEmail', newEmail)
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected')
   });
